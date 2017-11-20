a74ee87eb0b3116668eea2457d832e53bd3ce12c
Nicolai Willems <nwi@tradeshift.com>
2017-11-20 17:21:29 +0100

    Javascript building

---
+++ scripts/repo-item.vue
@@ -0,0 +1,20 @@
+<template>
+    <li>
+        <div><a v-on:click='selectRepo'>{{ repo.id }}</a></div>
+    </li>
+</template>
+
+<script>
+module.exports = {
+    props: ['repo'],
+    template: "#repo-item",
+    methods: {
+        selectRepo: function(){
+            this.$emit('reposelected', this.repo)
+        }
+    }
+};
+</script>
+
+<style>
+</style>
