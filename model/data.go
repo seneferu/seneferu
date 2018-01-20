6a2daa44399e6f49f7a6b6e9c162aa7f93561703
Soren Mathiasen <smo@tradeshift.com>
2018-01-20 16:40:12 +0100

    add go reportcard badge
    This will add a Go reportcard badge to the README.md, and fix some
    of the issues it detected

--- model/data.go
+++ model/data.go
@@ -4,6 +4,7 @@ import (
 	"time"
 )
 
+// Build is the structure describing a build in Seneferu
 type Build struct {
 	Org  string `json:"organisation"`
 	Name string `json:"name"`
@@ -22,6 +23,7 @@ type Build struct {
 	Ref string
 }
 
+// StepInfo contains information about each build step
 type StepInfo struct {
 	BuildNumber int    `json:"buildnumber"`
 	Org         string `json:"org"`
@@ -31,20 +33,24 @@ type StepInfo struct {
 	ExitCode    int32  `json:"exitcode"`
 }
 
+// Step contains step information and the log entry
 type Step struct {
 	StepInfo
 	Log string `json:"build"`
 }
 
+// Service is used to describe a service which is a thing builds can depend on
+// typically this could be postgres database or something like that
 type Service struct {
 	Name string `json:"name"`
 	Log  string `json:"build"`
 }
 
+// Repo basic information about a repository
 type Repo struct {
 	Org  string `json:"org"`
 	Name string `json:"name"`
-	Url  string `json:"url"`
+	URL  string `json:"url"`
 }
 
 // Deployment is a structure defining a Helm deployment
